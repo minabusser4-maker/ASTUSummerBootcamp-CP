a907494e57e623720fe7052d5f94b9ffc94b14d5
minabusser4-maker <minabusser4@gmail.com>
2026-06-27 02:18:48 +0500

    Refactor longestAlternatingSubarray implementation
    Refactor longestAlternatingSubarray method to improve logic and readability.

---
+++ Longest Even Odd Subarray With Threshold.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+        n = len(nums)
+        ans = 0
+        i = 0
+
+        while i < n:
+            if nums[i] % 2 == 1 or nums[i] > threshold:
+                i += 1
+                continue
+
+            j = i
+            while (
+                j + 1 < n
+                and nums[j + 1] <= threshold
+                and nums[j] % 2 != nums[j + 1] % 2
+            ):
+                j += 1
+
+            ans = max(ans, j - i + 1)
+            i += 1
+
+        return ans            
+        
